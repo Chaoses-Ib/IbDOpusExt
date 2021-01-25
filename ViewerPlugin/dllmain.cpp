6c2e610cf3d98a1ff5659b2fd8fc0dadd341aba4
Chaoses Ib <Chaos-es@outlook.com>
2021-01-25 19:27:50 +0800

    Update DLL

--- ViewerPlugin/dllmain.cpp
+++ ViewerPlugin/dllmain.cpp
@@ -6,7 +6,7 @@ using namespace std;
 
 void DebugOutput(wstring str) {
 #ifdef _DEBUG
-    OutputDebugStringW((L"Ib" + str).c_str());
+    OutputDebugStringW((L"Ib: " + str).c_str());
 #endif // _DEBUG
 }
 
@@ -26,17 +26,40 @@ BOOL APIENTRY DllMain( HMODULE hModule,
     return TRUE;
 }
 
-BOOL DVP_InitEx(LPDVPINITEXDATA pInitExData) {
+#define EXPORT extern "C" __declspec(dllexport) 
+
+EXPORT BOOL DVP_InitEx(DVPInitExData pInitExData) {
     DebugOutput(L"DVP_InitEx");
     return TRUE;
 }
 
-void DVP_Uninit(void) {
-    DebugOutput(L"DVP_InitEx");
+EXPORT void DVP_Uninit(void) {
+    DebugOutput(L"DVP_Uninit");
     return;
 }
 
-BOOL DVP_USBSafe(LPOPUSUSBSAFEDATA pUSBSafeData) {
+EXPORT BOOL DVP_USBSafe(OpusUSBSafeData* pUSBSafeData) {
     DebugOutput(L"DVP_USBSafe");
+    return TRUE;
+}
+
+EXPORT BOOL DVP_IdentifyW(DOpusViewerPluginInfoW* lpVPInfo) {
+    DebugOutput(L"DVP_IdentifyW");
+    if (lpVPInfo->cbSize < VIEWERPLUGINFILEINFOW_V4_SIZE)
+        return FALSE;
+    lpVPInfo->dwFlags = DVPFIF_ExtensionsOnly
+                      | DVPFIF_NoThumbnails
+                      | DVPFIF_NoFileInformation
+                      | DVPFIF_UseVersionResource  //only for version and copyright
+                      ;
+    //wcscpy_s(lpVPInfo->lpszHandleExts, lpVPInfo->cchHandleExtsMax, L".abc");
+
+    wcscpy_s(lpVPInfo->lpszName, lpVPInfo->cchNameMax, L"IbDOpusExt");
+    wcscpy_s(lpVPInfo->lpszDescription, lpVPInfo->cchDescriptionMax, L"An IbDOpusExt");
+    wcscpy_s(lpVPInfo->lpszURL, lpVPInfo->cchURLMax, L"https://github.com/Chaoses-Ib/IbDOpusExt");
+
+    // {CB2D22A9-6C03-4B5D-B264-60224E70A46D}
+    lpVPInfo->idPlugin = { 0xcb2d22a9, 0x6c03, 0x4b5d, { 0xb2, 0x64, 0x60, 0x22, 0x4e, 0x70, 0xa4, 0x6D } };
+
     return TRUE;
 }
